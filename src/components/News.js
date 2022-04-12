bec82eaf017bca707820924d5d9dc3676d730e0f
Kamran <jameelk809@gmail.com>
2022-04-13 00:20:32 +0530

    added dummy news api
    this will be deleted in future

--- src/components/News.js
+++ src/components/News.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is a news component.
-        <NewsItem/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container' my-3>
+                <h3>NewsMonkey - Top Headlines</h3>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title="myTitle" description="myDesc" />
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default News
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
+    let {title, description} = this.props;
     return (
       <div>
-        This is a News Item.
+        <div className="card" style={{width: "18rem"}}>
+          <img src="https://m.files.bbci.co.uk/modules/bbc-morph-sport-seo-meta/1.22.0/images/bbc-sport-logo.png" className="card-img-top" alt="..."/>
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
+              <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
       </div>
     )
   }
