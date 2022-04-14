07621626a93ade7319b087a365e80fd635c654d3
Kamran <jameelk809@gmail.com>
2022-04-14 23:21:44 +0530

    Router added, react-router-dom version degraded
    >> npm uninstall react-router-dom
    >> npm install react-router-dom@5.2.0

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import './App.css';
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 export default class App extends Component {
 
   render() {
     return (
       <div>
-        <NavBar/>
-        <News pageSize={6} country="in" category="science"/>
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/"><News key="general" pageSize={9} country="in" category="general" /></Route>
+            <Route exact path="/business"><News key="business" pageSize={9} country="in" category="business" /></Route>
+            <Route exact path="/entertainment"><News key="entertainment" pageSize={9} country="in" category="entertainment" /></Route>
+            <Route exact path="/general"><News key="general" pageSize={9} country="in" category="general" /></Route>
+            <Route exact path="/health"><News key="health" pageSize={9} country="in" category="health" /></Route>
+            <Route exact path="/science"><News key="science" pageSize={9} country="in" category="science" /></Route>
+            <Route exact path="/sports"><News key="sports" pageSize={9} country="in" category="sports" /></Route>
+            <Route exact path="/technology"><News key="technology" pageSize={9} country="in" category="technology" /></Route>
+          </Switch>
+        </Router>
       </div>
     )
   }
--- src/components/News.js
+++ src/components/News.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-import Spinner from './Spinner'
+import Spinner from './Spinner';
 import PropTypes from 'prop-types'
 
 
@@ -10,17 +10,15 @@ export class News extends Component {
         pageSize: 9,
         category: 'general',
     }
-    
+
     static propTypes = {
         country: PropTypes.string,
         pageSize: PropTypes.number,
         category: PropTypes.string,
     }
-    
 
     constructor() {
         super();
-        console.log("News Constructor");
         this.state = {
             articles: [],
             loading: false,
@@ -29,23 +27,24 @@ export class News extends Component {
     }
 
     async componentDidMount() {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=business&category${this.props.category}&apiKey=9563edd417894fcaafdc56ad77758f3f&page=1&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9563edd417894fcaafdc56ad77758f3f&page=1&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
-        let parsedData = await data.json();
+        let parsedData = await data.json()
         console.log(parsedData);
-        this.setState({ 
-            articles: parsedData.articles, 
-            totalResults: parsedData.totalResults, 
-            loading: false 
+        this.setState({
+            articles: parsedData.articles,
+            totalResults: parsedData.totalResults,
+            loading: false
         })
     }
 
     handlePrevClick = async () => {
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=business&category${this.props.category}&apiKey=9563edd417894fcaafdc56ad77758f3f&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        console.log("Previous");
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9563edd417894fcaafdc56ad77758f3f&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
-        let parsedData = await data.json();
+        let parsedData = await data.json()
         console.log(parsedData);
         this.setState({
             page: this.state.page - 1,
@@ -55,11 +54,12 @@ export class News extends Component {
     }
 
     handleNextClick = async () => {
+        console.log("Next");
         if (!(this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize))) {
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=business&category${this.props.category}&apiKey=9563edd417894fcaafdc56ad77758f3f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9563edd417894fcaafdc56ad77758f3f&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
             this.setState({ loading: true });
             let data = await fetch(url);
-            let parsedData = await data.json();
+            let parsedData = await data.json()
             this.setState({
                 page: this.state.page + 1,
                 articles: parsedData.articles,
@@ -70,18 +70,13 @@ export class News extends Component {
 
     render() {
         return (
-            <div className='container my-3'>
-                <h1 className="text-center" style={{margin: '35px 0px'}} >NewsMonkey - Top Headlines</h1>
-                {this.state.loading && <Spinner/>}
+            <div className="container my-3">
+                <h1 className="text-center" style={{ margin: '35px 0px' }}>NewsMonkey - Top Headlines</h1>
+                {this.state.loading && <Spinner />}
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element) => {
-                        return <div className="col-md-4" key={element.url} >
-                            <NewsItem
-                                title={element.title ? element.title : ""}
-                                description={element.description ? element.description : ""}
-                                imageUrl={element.urlToImage ? element.urlToImage : "http://shorturl.at/apxLX"}
-                                newsUrl={element.url}
-                            />
+                        return <div className="col-md-4" key={element.url}>
+                            <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
                 </div>
