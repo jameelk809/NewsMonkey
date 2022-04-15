52a6d81fa766013a3ddd151cbb5ad64632044ffd
Kamran <jameelk809@gmail.com>
2022-04-15 14:57:12 +0530

    top loading bar added
    >> npm i react-top-loading-bar --force

--- src/components/News.js
+++ src/components/News.js
@@ -31,16 +31,19 @@ export class News extends Component {
     }
 
     async updateNews() {
+        this.props.setProgress(10);
         const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=9563edd417894fcaafdc56ad77758f3f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({ loading: true });
         let data = await fetch(url);
+        this.props.setProgress(30);
         let parsedData = await data.json()
+        this.props.setProgress(70);
         this.setState({
             articles: parsedData.articles,
             totalResults: parsedData.totalResults,
             loading: false,
         })
-
+        this.props.setProgress(100);
     }
     async componentDidMount() {
         this.updateNews();
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,15 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card">
-          <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left: "90%", zIndex: 1}}>{source}</span>
+          <div style={{
+            display: 'flex',
+            justifyContent: 'flex-end',
+            position: 'absolute',
+            right: '0px'
+          }}>
+
+          <span className="badge rounded-pill bg-success" >{source}</span>
+          </div>
           <img src={imageUrl} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
