765bf1abc3fa366869e5a356ba2f61a47b5a37a6
Kamran <jameelk809@gmail.com>
2022-04-15 16:34:14 +0530

    changed class based components to functional based components

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,31 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export class NewsItem extends Component {
-  render() {
-    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-    return (
-      <div className='my-3'>
-        <div className="card">
-          <div style={{
-            display: 'flex',
-            justifyContent: 'flex-end',
-            position: 'absolute',
-            right: '0px'
-          }}>
+const NewsItem = (props) => {
+  let { title, description, imageUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className='my-3'>
+      <div className="card">
+        <div style={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          position: 'absolute',
+          right: '0px'
+        }}>
 
           <span className="badge rounded-pill bg-success" >{source}</span>
-          </div>
-          <img src={imageUrl} className="card-img-top" alt="..." />
-          <div className="card-body">
-            <h5 className="card-title">{title}...</h5>
-            <p className="card-text">{description}...</p>
-            <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()} </small></p>
-            <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
-          </div>
+        </div>
+        <img src={imageUrl} className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{title}...</h5>
+          <p className="card-text">{description}...</p>
+          <p className="card-text"><small className="text-muted">By {author} on {new Date(date).toGMTString()} </small></p>
+          <a href={newsUrl} target="_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+
 }
 
 export default NewsItem
